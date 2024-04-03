8a92121878a6b33487a386df2e39506bc260cfbe
Timm Reichert <treichert@TImm-Reicherts-MacBook-Pro.fritz.box>
2024-04-03 10:32:17 +0200

    changes

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,11 +6,11 @@ import createTag from './tag.js';
  * result: removed the undesired wrapper
  */
 export function removeOuterElementLayer(element, targetSelector) {
-  const targetElement = element.querySelector(targetSelector);
-  if (targetElement) {
-    const parent = targetElement.parentNode;
-    if (parent) (parent).replaceWith(targetElement);
-  }
+    const targetElement = element.querySelector(targetSelector);
+    if (targetElement) {
+        const parent = targetElement.parentNode;
+        if (parent) (parent).replaceWith(targetElement);
+    }
 }
 
 /**
@@ -28,19 +28,19 @@ export function removeOuterElementLayer(element, targetSelector) {
  * *         </div>
  */
 export function combineChildrenToSingleDiv(element) {
-  const targetChildren = element.querySelectorAll(':scope > div');
-  if (targetChildren.length === 0) { return; }
+    const targetChildren = element.querySelectorAll(':scope > div');
+    if (targetChildren.length === 0) { return; }
 
-  const singleDiv = document.createElement('div');
-  targetChildren.forEach((targetChild) => {
-    const children = Array.from(targetChild.childNodes);
-    children.forEach((childElement) => {
-      singleDiv.appendChild(childElement);
+    const singleDiv = document.createElement('div');
+    targetChildren.forEach((targetChild) => {
+        const children = Array.from(targetChild.childNodes);
+        children.forEach((childElement) => {
+            singleDiv.appendChild(childElement);
+        });
+        targetChild.remove();
     });
-    targetChild.remove();
-  });
 
-  element.append(singleDiv);
+    element.append(singleDiv);
 }
 
 /**
@@ -50,11 +50,11 @@ export function combineChildrenToSingleDiv(element) {
  * result: return the new element with inner content of the element, desired tag and css class
  */
 export function changeTag(element, targetTag, className) {
-  const newElClass = className || '';
-  const innerContent = element.innerHTML;
-  const newTagElement = createTag(targetTag, { class: newElClass }, innerContent);
+    const newElClass = className || '';
+    const innerContent = element.innerHTML;
+    const newTagElement = createTag(targetTag, { class: newElClass }, innerContent);
 
-  return newTagElement;
+    return newTagElement;
 }
 
 /**
@@ -62,18 +62,18 @@ export function changeTag(element, targetTag, className) {
  * result: return `_self` or `_blank` if the link has the same host
  */
 export function returnLinkTarget(url) {
-  const currentHost = window.location.host;
-  const urlObject = new URL(url);
-  const isSameHost = urlObject.host === currentHost;
+    const currentHost = window.location.host;
+    const urlObject = new URL(url);
+    const isSameHost = urlObject.host === currentHost;
 
-  // take in pathname that should be opened in new tab, in redirects excel
-  const redirectExternalPaths = ['/history', '/chat'];
-  const redirectToExternalPath = redirectExternalPaths.includes(urlObject.pathname);
+    // take in pathname that should be opened in new tab, in redirects excel
+    const redirectExternalPaths = ['/history', '/chat'];
+    const redirectToExternalPath = redirectExternalPaths.includes(urlObject.pathname);
 
-  if (!isSameHost || redirectToExternalPath) {
-    return '_blank';
-  }
-  return '_self';
+    if (!isSameHost || redirectToExternalPath) {
+        return '_blank';
+    }
+    return '_self';
 }
 
 // as the blocks are loaded in aysnchronously, we don't have a specific timing
@@ -83,94 +83,94 @@ export function returnLinkTarget(url) {
 const requireRevealWrapper = ['slide-reveal-up', 'slide-reveal-up-slow'];
 
 export function addRevealWrapperToAnimationTarget(element) {
-  const revealWrapper = createTag('div', { class: 'slide-reveal-wrapper' });
-  const parent = element.parentNode;
-  // Insert the wrapper before the element
-  parent.insertBefore(revealWrapper, element);
-  revealWrapper.appendChild(element);
+    const revealWrapper = createTag('div', { class: 'slide-reveal-wrapper' });
+    const parent = element.parentNode;
+    // Insert the wrapper before the element
+    parent.insertBefore(revealWrapper, element);
+    revealWrapper.appendChild(element);
 }
 
 // eslint-disable-next-line max-len
 export function addAnimatedClassToElement(targetSelector, animatedClass, delayTime, targetSelectorWrapper) {
-  const target = targetSelectorWrapper.querySelector(targetSelector);
-  if (target) {
-    target.classList.add(animatedClass);
-    if (delayTime) target.style.transitionDelay = `${delayTime}s`;
-    if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
-      addRevealWrapperToAnimationTarget(target);
+    const target = targetSelectorWrapper.querySelector(targetSelector);
+    if (target) {
+        target.classList.add(animatedClass);
+        if (delayTime) target.style.transitionDelay = `${delayTime}s`;
+        if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
+            addRevealWrapperToAnimationTarget(target);
+        }
     }
-  }
 }
 
 // eslint-disable-next-line max-len
 export function addAnimatedClassToMultipleElements(targetSelector, animatedClass, delayTime, targetSelectorWrapper, staggerTime) {
-  const targets = targetSelectorWrapper.querySelectorAll(targetSelector);
-  if (targets) {
-    targets.forEach((target, i) => {
-      target.classList.add(animatedClass);
-      if (delayTime) target.style.transitionDelay = `${delayTime * (i + 1)}s`;
-      if (staggerTime) target.style.transitionDelay = `${delayTime + staggerTime * (i + 1)}s`;
-      if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
-        addRevealWrapperToAnimationTarget(target);
-      }
-    });
-  }
+    const targets = targetSelectorWrapper.querySelectorAll(targetSelector);
+    if (targets) {
+        targets.forEach((target, i) => {
+            target.classList.add(animatedClass);
+            if (delayTime) target.style.transitionDelay = `${delayTime * (i + 1)}s`;
+            if (staggerTime) target.style.transitionDelay = `${delayTime + staggerTime * (i + 1)}s`;
+            if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
+                addRevealWrapperToAnimationTarget(target);
+            }
+        });
+    }
 }
 
 export function addInviewObserverToTriggerElement(triggerElement) {
-  const observerOptions = {
-    threshold: 0.25, // show when is 25% in view
-  };
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('in-view');
-        observer.unobserve(entry.target);
-      }
-    });
-  }, observerOptions);
-  observer.observe(triggerElement);
+    const observerOptions = {
+        threshold: 0.25, // show when is 25% in view
+    };
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+    observer.observe(triggerElement);
 }
 
 // eslint-disable-next-line max-len
 export function addInViewAnimationToSingleElement(targetElement, animatedClass, triggerElement, delayTime) {
-  // if it's HTML element
-  if (targetElement.nodeType === 1) {
-    targetElement.classList.add(animatedClass);
-    if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
-      addRevealWrapperToAnimationTarget(targetElement);
+    // if it's HTML element
+    if (targetElement.nodeType === 1) {
+        targetElement.classList.add(animatedClass);
+        if (requireRevealWrapper.indexOf(animatedClass) !== -1) {
+            addRevealWrapperToAnimationTarget(targetElement);
+        }
     }
-  }
-  // if it's string only, which should be a selector
-  if (targetElement.nodeType === 3) {
-    addAnimatedClassToElement(targetElement, animatedClass, triggerElement, delayTime);
-  }
-  const trigger = triggerElement || targetElement;
-  addInviewObserverToTriggerElement(trigger);
+    // if it's string only, which should be a selector
+    if (targetElement.nodeType === 3) {
+        addAnimatedClassToElement(targetElement, animatedClass, triggerElement, delayTime);
+    }
+    const trigger = triggerElement || targetElement;
+    addInviewObserverToTriggerElement(trigger);
 }
 
 export function addInViewAnimationToMultipleElements(animatedItems, triggerElement, staggerTime) {
-  // set up animation class
-  animatedItems.forEach((el, i) => {
-    const delayTime = staggerTime ? i * staggerTime : null;
-    if (Object.prototype.hasOwnProperty.call(el, 'selector')) {
-      addAnimatedClassToElement(el.selector, el.animatedClass, delayTime, triggerElement);
-    }
-    if (Object.prototype.hasOwnProperty.call(el, 'selectors')) {
-      // eslint-disable-next-line max-len
-      addAnimatedClassToMultipleElements(el.selectors, el.animatedClass, el.staggerTime, triggerElement);
-    }
-  });
+    // set up animation class
+    animatedItems.forEach((el, i) => {
+        const delayTime = staggerTime ? i * staggerTime : null;
+        if (Object.prototype.hasOwnProperty.call(el, 'selector')) {
+            addAnimatedClassToElement(el.selector, el.animatedClass, delayTime, triggerElement);
+        }
+        if (Object.prototype.hasOwnProperty.call(el, 'selectors')) {
+            // eslint-disable-next-line max-len
+            addAnimatedClassToMultipleElements(el.selectors, el.animatedClass, el.staggerTime, triggerElement);
+        }
+    });
 
-  // add `.in-view` to triggerElement, so the elements inside will start animating
-  addInviewObserverToTriggerElement(triggerElement);
+    // add `.in-view` to triggerElement, so the elements inside will start animating
+    addInviewObserverToTriggerElement(triggerElement);
 }
 
 export default {
-  removeOuterElementLayer,
-  changeTag,
-  returnLinkTarget,
-  addInViewAnimationToSingleElement,
-  addInViewAnimationToMultipleElements,
-  addInviewObserverToTriggerElement,
+    removeOuterElementLayer,
+    changeTag,
+    returnLinkTarget,
+    addInViewAnimationToSingleElement,
+    addInViewAnimationToMultipleElements,
+    addInviewObserverToTriggerElement,
 };
