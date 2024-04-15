e6ce9b3724e9e4e8edc8a272b8bc633d80507c3f
Timm Reichert <treichert@TImm-Reicherts-MacBook-Pro.fritz.box>
2024-04-15 15:56:50 +0200

    changes

--- tools/sidekick/plugins/accessibility-mode/accessibility-mode.js
+++ tools/sidekick/plugins/accessibility-mode/accessibility-mode.js
@@ -7,8 +7,10 @@ const createDialog = () => {
     createElement('div', { class: 'hlx-a11y-mode-dialog-container' }, [
       createElement('h4', { class: 'hlx-a11y-mode-dialog-title' }, 'Start Approval Process'),
       createElement('p', {}, 'Please select the role and approver'),
-      createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
-        createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Start Approval Process '),
+      createElement('div', { id: 'test-123' }, [
+        createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
+          createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Start Approval Process '),
+        ]),
       ]),
     ]),
   ]);
@@ -16,10 +18,8 @@ const createDialog = () => {
 };
 
 function createDropdown() {
-  // Erstellen des 'select'-Elements
   const select = document.createElement('select');
 
-  // Optionen für das Dropdown hinzufügen
   const options = ['Option 1', 'Option 2', 'Option 3'];
   options.forEach((optionText) => {
     const option = document.createElement('option');
@@ -28,7 +28,6 @@ function createDropdown() {
     select.appendChild(option);
   });
 
-  // Optionales Hinzufügen eines Event Listeners für das 'change' Ereignis
   select.addEventListener('change', function () {
     console.log('Ausgewählte Option:', this.value);
   });
@@ -40,7 +39,7 @@ const initAccessibilityMode = async () => {
   await loadCSS(`${window.hlx.codeBasePath}/tools/sidekick/plugins/accessibility-mode/accessibility-mode.css`);
 
   const approvalStartDialog = createDialog();
-  approvalStartDialog.appendChild(createDropdown());
+  document.getElementById('test-123').appendChild(createDropdown());
   document.body.appendChild(approvalStartDialog);
 
   const button = helpDialog.querySelector('.hlx-a11y-mode-dialog-button');
