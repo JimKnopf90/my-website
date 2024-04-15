3f972c7765f823a32cb01493461e18a1b6743003
Timm Reichert <treichert@TImm-Reicherts-MacBook-Pro.fritz.box>
2024-04-15 09:18:04 +0200

    test sidekick plugin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,6 +13,19 @@ import {
   loadCSS,
 } from './aem.js';
 
+const sk = document.querySelector('helix-sidekick');
+console.log('sdsdsdsdsd');
+if (sk) {
+  // sidekick already loaded
+  sk.addEventListener('custom:foo', foo);
+} else {
+  // wait for sidekick to be loaded
+  document.addEventListener('sidekick-ready', () => {
+    document.querySelector('helix-sidekick')
+      .addEventListener('custom:foo', foo);
+  }, { once: true });
+}
+
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
@@ -140,16 +153,3 @@ const foo = ({ detail }) => {
   console.log('Test 123');
   alert('sdsd');
 };
-
-const sk = document.querySelector('helix-sidekick');
-console.log('sdsdsdsdsd');
-if (sk) {
-  // sidekick already loaded
-  sk.addEventListener('custom:foo', foo);
-} else {
-  // wait for sidekick to be loaded
-  document.addEventListener('sidekick-ready', () => {
-    document.querySelector('helix-sidekick')
-      .addEventListener('custom:foo', foo);
-  }, { once: true });
-}
