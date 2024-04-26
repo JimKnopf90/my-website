3c457bdf20f413ef6e165ed71120bd59c9ce268e
Timm Reichert <treichert@TImm-Reicherts-MacBook-Pro.fritz.box>
2024-04-26 09:55:30 +0200

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -93,6 +93,7 @@ const accessibilityMode = async (e) => {
 const sk = document.querySelector('helix-sidekick');
 
 if (sk) {
+  console.log(sk)
   sk.addEventListener('custom:accessibility-mode', accessibilityMode);
 } else {
   document.addEventListener('sidekick-ready', () => {
--- tools/sidekick/plugins/approval-mode/approval-mode.js
+++ tools/sidekick/plugins/approval-mode/approval-mode.js
@@ -1,26 +1,70 @@
 /* eslint-disable no-undef, no-unused-vars */
 import { loadCSS } from '../../../../scripts/aem.js';
-import { createElement } from '../../../../scripts/scripts.js'; // eslint-disable-line import/no-cycle
+import { createElement } from '../../../../scripts/scripts.js'; // eslint-disable-line import/no-cycle;
+
+let selectedUser = '';
+let selectedUserId = '';
+let selectedRole = 'Approver';
+const selectedUserRole = [{ name: '', id: '', role: '' }];
 
 const createDialog = () => {
   const dialog = createElement('div', { id: 'hlx-a11y-mode-dialog' }, [
     createElement('div', { class: 'hlx-a11y-mode-dialog-container' }, [
       createElement('h4', { class: 'hlx-a11y-mode-dialog-title' }, 'Start Approval Process'),
       createElement('p', {}, 'Please select the role and approver'),
-      createElement('div', { id: 'test-123' }, [
-        createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
-          createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Start Approval Process '),
-        ]),
+      createElement('div', { id: 'selection' }, [
+        createElement('button', { id: 'btn-add', disabled: true }, '+')
       ]),
+      createElement('div', { id: 'result' }),
+      createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
+        createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Start Approval Process '),
+      ]),
+
     ]),
   ]);
   return dialog;
 };
 
-function createDropdown() {
-  const select = document.createElement('select');
+const add = () => {
+  selectedUserRole.push({ name: selectedUser, selectedRole: selectedRole });
+  const div = document.createElement('div');
+  div.id = "result-list";
+  const divName = document.createElement('div');
+  divName.innerText = `Name: ${selectedUser}`;
+  const divRole = document.createElement('div');
+  divRole.id = "result-role";
+  divRole.innerText = selectedRole;
+  div.append(divName);
+  div.append(divRole);
+  document.getElementById('result').appendChild(div);
 
-  const options = ['Option 1', 'Option 2', 'Option 3'];
+  selectedRole = 'Approver';
+  document.getElementById('btn-add').disabled = true;
+  document.getElementById('role-selection').disabled = true;
+}
+
+const fetchUsers = async () => {
+  let userList = [];
+  await fetch('https://rest.proofhq.eu/api/v1/contacts', {
+    headers: {
+      "Content-Type": "application/json",
+      "Sessionid": "03E1NDY5YmM4MjkzYmE5M2FiMWY0M2YzMDgxYTkr",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.map(user => userList.push({ name: user.firstName + " " + user.lastName, id: user.accountToken }))
+    })
+  return userList;
+}
+
+function createRoleDropdown() {
+  const select = document.createElement('select');
+  select.classList = 'custom-select';
+  select.id = "role-selection"
+  select.disabled = true;
+
+  const options = ['Approver', 'Reviewer and Approver', 'Author', 'Moderator', 'Read only'];
   options.forEach((optionText) => {
     const option = document.createElement('option');
     option.textContent = optionText;
@@ -29,7 +73,30 @@ function createDropdown() {
   });
 
   select.addEventListener('change', function () {
-    console.log('Ausgewählte Option:', this.value);
+    selectedRole = this.value;
+    document.getElementById('btn-add').disabled = false;
+  });
+
+  return select;
+}
+
+async function createUserDropdown() {
+  const userList = await fetchUsers();
+
+  const select = document.createElement('select');
+  select.classList = 'custom-select';
+
+
+  userList.forEach((user) => {
+    const option = document.createElement('option');
+    option.textContent = user.name;
+    option.value = user.name;
+    select.appendChild(option);
+  });
+
+  select.addEventListener('change', function () {
+    selectedUser = this.value;
+    document.getElementById('role-selection').disabled = false;
   });
 
   return select;
@@ -40,13 +107,44 @@ const initAccessibilityMode = async () => {
 
   const approvalStartDialog = createDialog();
   document.body.appendChild(approvalStartDialog);
-  document.getElementById('test-123').prepend(createDropdown());
+  const userDropDown = await createUserDropdown();
+  document.getElementById('selection').prepend(createRoleDropdown());
+  document.getElementById('selection').prepend(userDropDown);
 
-  const button = helpDialog.querySelector('.hlx-a11y-mode-dialog-button');
+  const button = approvalStartDialog.querySelector('.hlx-a11y-mode-dialog-button');
 
   button.addEventListener('click', () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify([
+      {
+        "contactToken": "dec79531pe5877710e79336b9ueb6f9c71d",
+        "role": -1
+      },
+      {
+        "contactToken": "c6adba50p99980248ee0369eaudf702c102",
+        "role": 4
+      }
+    ]);
+
+    const requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://localhost:8080/api/approvals?pageUrl=https://main--my-website--jimknopf90.hlx.page", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+
     approvalStartDialog.remove();
   });
+
+  const addButton = document.getElementById('btn-add');
+  addButton.addEventListener('click', add, false);
 };
 
 export default initAccessibilityMode;
