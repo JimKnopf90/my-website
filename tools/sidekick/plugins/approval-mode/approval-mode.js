0448c2f4546bf8fdf7e94949d3faf9e7ac8d5114
Timm Reichert <t.reichert90@outloook.de>
2024-05-28 09:08:45 +0200

    ui

--- tools/sidekick/plugins/approval-mode/approval-mode.js
+++ tools/sidekick/plugins/approval-mode/approval-mode.js
@@ -2,11 +2,7 @@
 import { loadCSS } from '../../../../scripts/aem.js';
 import { createElement } from '../../../../scripts/scripts.js';
 
-const baseUrl = 'https://95ed-2003-f9-872f-f800-4584-666c-3a37-38ef.ngrok-free.app';
-const selectedUser = '';
-const selectedUserId = '';
-let selectedRole = 'Approver';
-let selectedRoleId = 4;
+const baseUrl = 'http://localhost:8080';
 let selectedUserRole = [];
 const roles = [
   { role: 'Approver', value: 4 },
@@ -16,75 +12,68 @@ const roles = [
   { role: 'Read only', value: 1 },
 ];
 
-function getRoleNameById(roleId) {
-  const role = roles.find((r) => r.value === roleId);
-  if (role) {
-    return role.role;
-  }
-  return 'Role not found';
+function createRoleDropdown() {
+  const select = document.createElement('select');
+  select.classList = 'custom-select';
+  select.id = 'role-selection';
+
+  roles.forEach((role) => {
+    const option = document.createElement('option');
+    option.textContent = role.role;
+    option.value = role.value;
+    select.appendChild(option);
+  });
+
+  return select;
 }
 
-const add2 = (name, contactToken, role) => {
-  selectedUserRole.push({ contactToken, role });
+function createWorkflowParticipants() {
+  const approvers = document.getElementById('approvers');
 
-  const div = document.createElement('div');
-  div.id = 'result-list';
+  while (approvers.rows.length > 1) {
+    approvers.deleteRow(1);
+  }
 
-  const divName = document.createElement('div');
-  divName.innerText = `Name: ${name}`;
+  selectedUserRole.forEach((userRole, index) => {
+    const row = approvers.insertRow(index + 1);
+    const name = row.insertCell(0);
+    const role = row.insertCell(1);
+    name.innerHTML = userRole.name;
 
-  const divRole = document.createElement('div');
-  divRole.id = 'result-role';
-  divRole.innerText = getRoleNameById(role);
+    const roleDropdown = createRoleDropdown();
+    roleDropdown.value = userRole.role;
+    roleDropdown.disabled = false;
 
-  div.append(divName);
-  div.append(divRole);
-  document.getElementById('result').appendChild(div);
+    roleDropdown.addEventListener('change', function () {
+      userRole.role = this.value;
+    });
+
+    role.appendChild(roleDropdown);
+  });
+}
+
+const add = (name, contactToken, role) => {
+  document.getElementById('search').value = '';
+  document.getElementById('hint').hidden = false;
+  document.getElementById('hit-list').hidden = true;
+  selectedUserRole.push({ name, contactToken, role });
+  document.getElementById('btn-start-approval').disabled = false;
+
+  createWorkflowParticipants();
 };
 
 function createUserHitList(userList) {
   const hitList = document.getElementById('hit-list');
   userList.forEach((user) => {
     const entry = document.createElement('div');
-    entry.textContent = `${user.name} - ${user.email} `;
-    entry.addEventListener('click', () => add2(user.name, user.id, 4), false);
+    entry.className = 'hallo';
+    entry.textContent = `${user.name} (${user.email}) `;
+    entry.addEventListener('click', () => add(user.name, user.id, 4), false);
     hitList.appendChild(entry);
   });
 }
 
-const add = () => {
-  selectedUserRole.push({ contactToken: selectedUserId, role: selectedRoleId });
-  const div = document.createElement('div');
-  div.id = 'result-list';
-  const divName = document.createElement('div');
-  divName.innerText = `Name: ${selectedUser}`;
-  const divRole = document.createElement('div');
-  divRole.id = 'result-role';
-  divRole.innerText = selectedRole;
-  div.append(divName);
-  div.append(divRole);
-  document.getElementById('result').appendChild(div);
-
-  selectedRole = 'Approver';
-  document.getElementById('btn-add').disabled = true;
-  document.getElementById('role-selection').disabled = true;
-};
-
-const fetchUsers = async () => {
-  const userList = [];
-  await fetch('http://localhost:8080/api/approvers?value=Pat', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.contacts.map((user) => userList.push({ name: user.name, id: user.token }));
-    });
-  return userList;
-};
-
-const fetchUsers2 = async (query) => {
+const fetchUsers = async (query) => {
   const response = await fetch(`${baseUrl}/api/approvers?value=${encodeURIComponent(query)}`, {
     headers: {
       'Content-Type': 'application/json',
@@ -94,42 +83,24 @@ const fetchUsers2 = async (query) => {
   return data.contacts.map((user) => ({ name: user.name, email: user.email, id: user.token }));
 };
 
-function createRoleDropdown() {
-  const select = document.createElement('select');
-  select.classList = 'custom-select';
-  select.id = 'role-selection';
-  select.disabled = true;
-
-  roles.forEach((role) => {
-    const option = document.createElement('option');
-    option.textContent = role.role;
-    option.value = role.value;
-    select.appendChild(option);
-  });
-
-  select.addEventListener('change', function () {
-    const roleName = this.options[this.selectedIndex].textContent;
-    selectedRole = roleName;
-    selectedRoleId = this.value;
-    document.getElementById('btn-add').disabled = false;
-  });
-
-  return select;
-}
-
 const createDialog = () => {
-  const dialog = createElement('div', { id: 'hlx-a11y-mode-dialog' }, [
+  const dialog = createElement('div', { id: 'hlx-approval-dialog' }, [
     createElement('div', { class: 'hlx-a11y-mode-dialog-container' }, [
       createElement('h4', { class: 'hlx-a11y-mode-dialog-title' }, 'Start Approval Process'),
-      createElement('p', {}, 'Please select the approver and role'),
-      createElement('input', { id: 'search', type: 'text', placeholder: 'Search for approvers' }),
-      createElement('div', { id: 'hit-list' }),
-      createElement('div', { id: 'selection' }, [
-        // createElement('button', { id: 'btn-add', disabled: true }, '+'),
+      createElement('p', {}, 'Search for approver and add a role'),
+      createElement('div', { id: 'result' }, [
+        createElement('table', { id: 'approvers' }, [
+          createElement('tr', {}, [
+            createElement('th', { class: 'col' }, 'Name or email address of the recipient'),
+            createElement('th', { class: 'col' }, 'Role'),
+          ]),
+        ]),
       ]),
-      createElement('div', { id: 'result' }),
+      createElement('input', { id: 'search', type: 'text', placeholder: 'Type the contacts name or email address to add a recipient' }),
+      createElement('div', { id: 'hit-list', hidden: 'true' }),
+      createElement('p', { id: 'hint' }, 'Type at least 3 characters'),
       createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
-        createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Start Approval Process '),
+        createElement('button', { class: 'hlx-a11y-mode-dialog-button', id: 'btn-start-approval', disabled: 'true' }, 'Start Approval Process '),
       ]),
     ]),
   ]);
@@ -139,50 +110,29 @@ const createDialog = () => {
     const { value } = searchInput;
     if (value.length >= 3) {
       document.getElementById('hit-list').innerHTML = '';
-      const userList = await fetchUsers2(value);
+      document.getElementById('hit-list').hidden = false;
+      document.getElementById('hint').hidden = true;
+      const userList = await fetchUsers(value);
       createUserHitList(userList);
+    } else {
+      document.getElementById('hint').hidden = false;
+      document.getElementById('hit-list').hidden = true;
     }
   });
 
   return dialog;
 };
 
-// async function createUserDropdown() {
-//   const userList = await fetchUsers();
-
-//   const select = document.createElement('select');
-//   select.classList = 'custom-select';
-
-//   userList.forEach((user) => {
-//     const option = document.createElement('option');
-//     option.textContent = user.name;
-//     option.value = user.id;
-//     select.appendChild(option);
-//   });
-
-//   select.addEventListener('change', function () {
-//     const name = this.options[this.selectedIndex].textContent;
-//     selectedUser = name;
-//     selectedUserId = this.value;
-//     document.getElementById('role-selection').disabled = false;
-//   });
-
-//   return select;
-// }
-
 const initApprovalMode = async (sourceLocation, previewUrl) => {
-  await loadCSS(`${window.hlx.codeBasePath}/tools/sidekick/plugins/approval-mode/accessibility-mode.css`);
+  await loadCSS(`${window.hlx.codeBasePath}/tools/sidekick/plugins/approval-mode/approval.css`);
 
   const approvalStartDialog = createDialog();
   document.body.appendChild(approvalStartDialog);
-  // const userDropDown = await createUserDropdown();
-
-  // document.getElementById('selection').prepend(createRoleDropdown());
-  // document.getElementById('selection').prepend(userDropDown);
 
   const button = approvalStartDialog.querySelector('.hlx-a11y-mode-dialog-button');
 
   button.addEventListener('click', () => {
+    button.disabled = true;
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify(selectedUserRole);
@@ -200,9 +150,6 @@ const initApprovalMode = async (sourceLocation, previewUrl) => {
 
     selectedUserRole = [];
   });
-
-  const addButton = document.getElementById('btn-add');
-  addButton.addEventListener('click', add, false);
 };
 
 export default initApprovalMode;
